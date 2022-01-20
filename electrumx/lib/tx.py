4d40c5ab504462682c601f5b26f4d77672bb4cf8
Yuriko Inaba <yurikoainaba@gmail.com>
2022-01-20 15:55:52 -0500

    Fix remove replay protection

--- electrumx/lib/tx.py
+++ electrumx/lib/tx.py
@@ -32,7 +32,7 @@ from hashlib import blake2s
 from typing import Sequence
 
 from electrumx.lib.hash import sha256, double_sha256, hash_to_hex_str
-from electrumx.lib.script import OpCodes
+from electrumx.lib.script import OpCodes, Script
 from electrumx.lib.util import (
     unpack_le_int32_from, unpack_le_int64_from, unpack_le_uint16_from,
     unpack_be_uint16_from,
